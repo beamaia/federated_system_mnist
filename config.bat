7b4895ccff317e867a14f0bf5833f3335bcfbd9d
beamaia <beamaia.work@gmail.com>
2023-05-20 16:48:04 -0300

    add scripts to create proto py
    Co-Authored-By: Sophie Dilhon <51868657+AHalic@users.noreply.github.com>

---
+++ config.bat
@@ -0,0 +1,2 @@
+python -m grpc_tools.protoc --proto_path=proto proto/server.proto --python_out=. --grpc_python_out=.
+python -m grpc_tools.protoc --proto_path=proto proto/client.proto --python_out=. --grpc_python_out=.
