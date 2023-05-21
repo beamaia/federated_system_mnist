913d0429f3244b0ca07697724a79d2e44165659f
beamaia <beamaia.work@gmail.com>
2023-05-20 23:46:58 -0300

    train and test
    Co-Authored-By: Sophie Dilhon <51868657+AHalic@users.noreply.github.com>
    Co-Authored-By: IagoSC <21135637+IagoSC@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
+# GRPC imports
 import grpc
 import server_pb2 
 import server_pb2_grpc
-
 import client_pb2 
 import client_pb2_grpc
-
 from concurrent import futures
 
-from random import sample, randint
+import threading
 
-def server():
-    grpc_server = grpc.server(futures.ThreadPoolExecutor(max_workers=8))
-    server_pb2_grpc.add_apiServicer_to_server(TrainerAgregator(), grpc_server)
-    grpc_server.add_insecure_port('localhost:8080')
-    grpc_server.start()
-    grpc_server.wait_for_termination()
+# Other imports
+from random import sample, randint
+import time
+import argparse
+
+# Custom imports
+from custom_thread import CustomThread
+from model import define_model, ModelBase64Encoder, ModelBase64Decoder
+
 
 '''
 opensTrainin
@@ -25,98 +27,144 @@ calculate_average
 
 repeat?
 '''
-import threading
-
-global MIN_CLIENTS_PER_ROUND
-global MIN_CLIENTS_TOTAL
-global MAX_ROUNDS
-
-class SemaphoredList:
-    list = []
-
-    
 
 class TrainerAgregator(server_pb2_grpc.apiServicer):     
     trainers = []
-    TRAINING_MODE = False
+    training_mode = False
     current_round = 0
 
-    def __init__(self):
-        self.current_round = 1
+    def __init__(self, min_clients_per_round=2, max_clients_total=5, max_rounds=10, \
+                 accuracy_threshold = 0.9, input_shape=(28, 28, 1), num_classes=10):
+        
+        # Configuring federated learning parameters
+        self.min_clients_per_round = min_clients_per_round
+        self.max_clients_total = max_clients_total
+        self.max_rounds = max_rounds
+        self.accuracy_threshold = accuracy_threshold
 
+        # Starting base model with no pretrained weights
+        self.model = define_model(input_shape, num_classes)
+
+        # Starting train function that verifies if training mode is on
         threading.Thread(target=self.train).start()
 
-
+    def federeated_train(self, results):
+        weights_list = [[weight.numpy() for weight in ModelBase64Decoder(result.weights).weights] for result in results]
+        # media do array
+        new_weights = [sum(weights)/len(weights) for weights in zip(*weights_list)]
+        return new_weights
 
     def train(self):
         while True:
-            if self.TRAINING_MODE:
-                print("Training model.")
-    # def check_init():
-    #     while True:
-    #         sem.acquire()
-    #         if len(trainers) > MIN_CLIENTS_PER_ROUND :
-                
-        
-    def connect_to_trainer(self):
-        print("Connecting to trainer.")
-        if len(self.trainers) == 0:
-            print("No trainers connected.")
-            return False
-        else:
-            print("Trainer connected.")
-            try:
-                info = self.trainers[0]
-                channel = grpc.insecure_channel(info["ipv4"] + ":" + str(info["port"]))
-                stub = client_pb2_grpc.apiStub(channel)
+            if self.training_mode:
+                for round in range(self.max_rounds):
+                    print("Starting round {}.".format(round))
+                    
+                    self.current_round = round
+                    results = self.init_round()
+                    
+                    # Calculate fed avg and update weights
+                    avg_weights = self.federeated_train(results)
+                    self.model.set_weights(avg_weights)
 
-                return stub.train_model(client_pb2.void())
-            except Exception as e:
-                print(e)
-                return False
-                
+                    self.test_round()
+                    # send model to all clients
+                    # receive metrics from clients
+                    # compares model metrics with threshold
+                    # if metrics are good enough, stop training
+                    
+                self.training_mode = False
+            time.sleep(5)
+
+    def test_round(self):
+      
+        threads = []
+        for trainer in self.trainers:
+            print(trainer)
+            threads.append(CustomThread(target=self.train_models, args=(trainer)))
+
+        for thread in threads:
+            thread.start()
+        
+        for thread in threads:
+            thread.join()   
+
+        return [t.value for t in threads]
+    
+    def test_models(self, ipv4, port, uuid):
+        channel = grpc.insecure_channel(ipv4 + ":" + str(port))
+        stub = client_pb2_grpc.apiStub(channel)
+        model_weights = ModelBase64Encoder(self.model)
+        return stub.test_model(client_pb2.models_weights_input(weights=model_weights, round_number=self.current_round))
+        
+
+    def train_models(self, ipv4, port, uuid):
+        channel = grpc.insecure_channel(ipv4 + ":" + str(port))
+        stub = client_pb2_grpc.apiStub(channel)
+        model_weights = ModelBase64Encoder(self.model)
+        return stub.train_model(client_pb2.models_weights_input(weights=model_weights, round_number=self.current_round))
+        
+
     def init_round(self):
-        selected_trainers = sample(self.trainers, randint(MIN_CLIENTS_PER_ROUND, len(self.trainers)))
-        
-        # add pra func de threads
+        print("Initializing round.")
+        number_of_trainers =  randint(self.min_clients_per_round, len(self.trainers))
+        selected_trainers = sample(self.trainers, number_of_trainers)
+        print(selected_trainers, number_of_trainers)
+        threads = []
         for trainer in selected_trainers:
-            channel = grpc.insecure_channel(trainer["ipv4"] + ":" + str(trainer["port"]))
-            stub = client_pb2_grpc.apiStub(channel)
-            stub.train_model(client_pb2.void())
+            print(trainer)
+            threads.append(CustomThread(target=self.train_models, args=(trainer)))
 
-        pass
+        for thread in threads:
+            thread.start()
+        
+        for thread in threads:
+            thread.join()   
 
-    def add_trainer(self, request, context):
+        return [t.value for t in threads]
+
+
+    def add_trainer(self, request, context):        
         self.trainers.append({
             "uuid": request.uuid,
             "port": request.port,
             "ipv4": request.ipv4
         })
         
-
         print("Trainer added.")
-        print(self.trainers)
+        print(self.trainers[-1])
 
-        if len(self.trainers) == 2: self.TRAINING_MODE = True
-
-        # se chegou na quantidade necessaria para treino, inicia
-        # e bloquea o recebimento de novos treinadores
-        self.connect_to_trainer()
-        print("Finished training")
+        if len(self.trainers) == self.max_clients_total: self.training_mode = True
         
-        return server_pb2.success(result=True)
+        return server_pb2.success(result=1)
 
-    # def load_data(): pass
-    # def load_data(): pass
+
+def parse_args():
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Start the server.')
+    parser.add_argument('--min_clients_per_round', type=int, default=2, help='Minimum number of clients per round.')
+    parser.add_argument('--max_clients_total', type=int, default=3, help='Maximum number of clients per round.')
+    parser.add_argument('--max_rounds', type=int, default=10, help='Maximum number of rounds.')
+    parser.add_argument('--accuracy_threshold', type=float, default=0.9, help='Minimum accuracy threshold.')
+    args = parser.parse_args()
+    return args
+
+def server(args):
+    grpc_server = grpc.server(futures.ThreadPoolExecutor(max_workers=8))
+    server_pb2_grpc.add_apiServicer_to_server(
+                                TrainerAgregator(min_clients_per_round=args.min_clients_per_round,
+                                                 max_clients_total=args.max_clients_total,
+                                                 max_rounds=args.max_rounds,
+                                                 accuracy_threshold=args.accuracy_threshold), grpc_server)
+    grpc_server.add_insecure_port('localhost:8080')
+    grpc_server.start()
+    grpc_server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    MIN_CLIENTS_PER_ROUND = 3
-    MIN_CLIENTS_TOTAL = 5
-    MAX_ROUNDS = 10
-    
+    args = parse_args()
     print("Starting server. Listening on port 8080.")
-    server()
+    server(args)
 
 
    
